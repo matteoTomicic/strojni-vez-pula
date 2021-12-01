72a378209e969d430091c3211200028d1d77ebb8
Matteo Tomicic <matteo.tomicic@convergent-usa.com>
2021-12-01 22:02:18 +0100

    added dynamical navigation render

--- src/components/Globals/Navigation/Navigation.jsx
+++ src/components/Globals/Navigation/Navigation.jsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 import { StyledNavigation } from "./Navigation.style";
 import Image from "next/image";
 import images from "../../../constants/image.constants";
+import navLinks from "../../../constants/navigation.constants";
 
 const Navigation = () => {
     const router = useRouter();
@@ -35,18 +36,13 @@ const Navigation = () => {
                         <Image src={images.logo.src} width={images.logo.width} height={images.logo.height} alt={images.logo.alt} title={images.logo.title} priority />
                     </div>
                     <nav className={`nav__items ${open ? "nav__items--shown" : ""}`}>
-                        <a href="/" title="Početna" className={`nav__item ${isPageActive("/")}`}>
-                            Početna
-                        </a>
-                        <a href="/o-nama" title="O nama" className={`nav__item ${isPageActive("/o-nama")}`}>
-                            O nama
-                        </a>
-                        <a href="/nasi-radovi" title="Naši radovi" className={`nav__item ${isPageActive("/nasi-radovi")}`}>
-                            Naši radovi
-                        </a>
-                        <a href="/kontakt" title="Kontakt" className={`nav__item ${isPageActive("/kontakt")}`}>
-                            Kontakt
-                        </a>
+                        {navLinks.map((navItem, index) => {
+                            return (
+                                <a key={index} href={navItem.path} title={navItem.title} className={`nav__item ${isPageActive(navItem.path)}`}>
+                                    {navItem.name}
+                                </a>
+                            );
+                        })}
                     </nav>
                 </div>
             </header>
---
+++ src/constants/navigation.constants.js
@@ -0,0 +1,24 @@
+const navLinks = [
+    { 
+        name: "Početna",
+        path: "/",
+        title: "Početna"
+    },
+    {
+        name: "O nama",
+        path: "/o-nama",
+        title: "O nama"
+    },
+    {
+        name: "Naši radovi",
+        path: "/nasi-radovi",
+        title: "Naši radovi"
+    },
+    {
+        name: "Kontakt",
+        path: "/kontakt",
+        title: "Kontakt"
+    }
+];
+
+export default navLinks;
